910b559dd0c761aac9dead4aaab5fda4f8cf6849
Phearak Both Bunna <phearakboth.bunna@wsu.edu>
2025-02-20 16:32:33 -0800

    updated animations

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,52 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn } from "./animation";
+import animations from "./animation";
+
+
+// const skillInfo = [
+//     // Programming languages
+//     {
+//         title: "Programming Languages",
+//         items: [
+//             {icon: "bi bi-code-slash" , text: "Python"},
+//             {icon: "bi bi-database", text: "SQL"},
+//             {icon: "bi bi-braces", text: "JavaScript"},
+//             {icon: "bi bi-filetype-html", text: "HTML"},
+//             {icon: "bi bi-filetype-css", text: "CSS"}
+//         ]
+
+//     },
+
+//     // Technical skills
+//     {
+//         title: "Technical Skills",
+//         items: [
+//             {icon: "bi bi-tools" , text: "Technical Support & Troubleshooting"},
+//             {icon: "bi bi-pc-display", text: "Helpdesk Operations"},
+//             {icon: "bi bi-graph-up-arrow", text: "Data Analytics"},
+//             {icon: "bi bi-globe", text: " Web/Mobile App Development"},
+//         ]
+//     },
+
+//     //  Tools & Tech
+//     {
+//         title: "Tools & Technologies",
+//         items: [
+//             {icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" , text: "React"},
+//             {icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg", text: "Azure"},
+//             {icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg", text: "Jira"},
+//         ]
+//     },
+
+//     // Certifications
+//     {
+//         title: "Certifications",
+//         items: [
+//             {icon: "bi bi-patch-check-fill fs-3" , text: "Google Cloud Essentials"},
+//             {icon: "bi bi-patch-check fs-3", text: "CompTIA A+ (In-progress)"},
+//         ]
+//     },    
+// ]
 
 const Skills = () => {
     return (
@@ -10,25 +56,18 @@ const Skills = () => {
             whileInView="visible"
             // animation starts when 20% are in the viewport
             viewport={{ amount: 0.2 }}
-            variants={{
-                hidden: {},
-                visible: {
-                    transition: {
-                        staggerChildren: 0.1
-                    }
-                }
-            }}>
+            variants={animations.fadeIn}>
 
             <div className="container p-5">
-                <motion.h2 className="text-center mb-4" variants={fadeIn}>
+                <motion.h2 className="text-center mb-4" variants={animations.fadeIn}>
                     Skills
                 </motion.h2>
-                <motion.div className="row gy-4 text-center" variants={fadeIn}>
+                <motion.div className="row gy-4 text-center" variants={animations.fadeIn}>
                     {/* Programming Languages */}
                     <div className="col-md-6">
-                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={fadeIn}>
+                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={animations.fadeIn}>
                             <h3 className="fw-bold">Programming Languages</h3>
-                            <motion.ul variants={fadeIn} className="list-unstyled fw-bold">
+                            <motion.ul variants={animations.fadeIn} className="list-unstyled fw-bold">
                                 <motion.li><i className="bi bi-code-slash"></i> Python</motion.li>
                                 <motion.li><i className="bi bi-database"></i> SQL</motion.li>
                                 <motion.li><i className="bi bi-braces"></i> JavaScript</motion.li>
@@ -40,9 +79,9 @@ const Skills = () => {
 
                     {/* Technical Skills */}
                     <div className="col-md-6">
-                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={fadeIn}>
+                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={animations.fadeIn}>
                             <h3 className="fw-bold">Technical Skills</h3>
-                            <motion.ul variants={fadeIn} className="list-unstyled fw-bold">
+                            <motion.ul variants={animations.fadeIn} className="list-unstyled fw-bold">
                                 <motion.li><i className="bi bi-tools"></i> Technical Support & Troubleshooting</motion.li>
                                 <motion.li><i className="bi bi-pc-display"></i> Helpdesk Operations</motion.li>
                                 <motion.li><i className="bi bi-graph-up-arrow"></i> Data Analytics</motion.li>
@@ -53,9 +92,9 @@ const Skills = () => {
 
                     {/* Frameworks and Tools */}
                     <div className="col-md-6 mb-2">
-                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={fadeIn}>
+                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={animations.fadeIn}>
                             <h3 className="fw-bold">Tools & Technologies</h3>
-                            <motion.ul variants={fadeIn} className="list-unstyled fw-bold">
+                            <motion.ul variants={animations.fadeIn} className="list-unstyled fw-bold">
                                 <motion.li>
                                     <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" width="30" /> React
                                 </motion.li>
@@ -71,9 +110,9 @@ const Skills = () => {
 
                     {/* Certifications */}
                     <div className="col-md-6 mb-2">
-                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={fadeIn}>
+                        <motion.div className="container custom-container border border-dark p-4 h-100" variants={animations.fadeIn}>
                             <h3 className="fw-bold">Certifications</h3>
-                            <motion.ul variants={fadeIn} className="list-unstyled fw-bold">
+                            <motion.ul variants={animations.fadeIn} className="list-unstyled fw-bold">
                                 <motion.li><i className="bi bi-patch-check-fill fs-3"></i> Google Cloud Essentials</motion.li>
                                 <motion.li><i className="bi bi-patch-check fs-3"></i> CompTIA A+ (In-progress)</motion.li>
                             </motion.ul>
